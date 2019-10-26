a84bd8914c72e1cfb622a5bd0b8389b0756286ce
B4gus <37102841+B4gus@users.noreply.github.com>
2019-10-27 02:39:15 +0700

    Translate bahasa

--- terkey.py
+++ terkey.py
@@ -7,12 +7,12 @@ b ='\033[91m'
 c ='\033[0m'
 os.system('clear')
 print(a+'\t  Shorcut for help you')
-print(b+'\t    Karjok Pangesty')
+print(b+'\t  Karjok Pangesty terima kasih ke dia ')
 print('\t https://t.me/CRABS_ID')
 print(a+'+'*40)
 print('\nProses..')
 sleep(1)
-print(b+'\n[!] making termux properties directory..')
+print(b+'\n[!] membuat termuk directory')
 sleep(1)
 try:
       os.mkdir('/data/data/com.termux/files/home/.termux')
@@ -20,7 +20,7 @@ except:
       pass
 print(a+'[!]Success !')
 sleep(1)
-print(b+'\n[!] Making setup file..')
+print(b+'\n[!] Membuat file konfigurasi..')
 sleep(1)
 
 key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
@@ -28,12 +28,12 @@ kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
 kontol.write(key)
 kontol.close()
 sleep(1)
-print(a+'[!] Success !')
+print(a+'[!] berhasil !')
 sleep(1)
-print(b+'\n[!] Setting up..')
+print(b+'\n[!] sedang menyeting..')
 sleep(2)
 os.system('termux-reload-settings')
-print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi https://t.me/om_karjok untuk requests\natau pertanyaan, atau hubungi https://t.me/CRABS_ID\nThanks :*\n\n')
+print(a+'[!] gud !! ^^'+c+'\n\nhubungi https://t.me/om_karjok untuk requests\natau pertanyaan, atau hubungi https://t.me/CRABS_ID\nThanks :*\n\n')
 
 
 # ini cuma shortcut buat bantu para nub
