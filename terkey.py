e03d396e4f7ecf762fb198b5a1fdefe03b54c0ca
karjok pangesty <narko.karjok@gmail.com>
2020-02-11 00:04:16 +0700

    change code, add loading animation

--- terkey.py
+++ terkey.py
@@ -5,36 +5,35 @@ from time import sleep
 a ='\033[92m'
 b ='\033[91m'
 c ='\033[0m'
-os.system('clear')
-print(a+'\t  Shorcut for help you')
-print(b+'\t    Karjok Pangesty')
-print('\t https://t.me/CRABS_ID')
-print(a+'+'*40)
-print('\nProses..')
-sleep(1)
-print(b+'\n[!] making termux properties directory..')
-sleep(1)
-try:
-      os.mkdir('/data/data/com.termux/files/home/.termux')
-except:
-      pass
-print(a+'[!]Success !')
-sleep(1)
-print(b+'\n[!] Making setup file..')
-sleep(1)
 
-key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
-kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
-kontol.write(key)
-kontol.close()
-sleep(1)
-print(a+'[!] Success !')
-sleep(1)
-print(b+'\n[!] Setting up..')
-sleep(2)
-os.system('termux-reload-settings')
-print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi https://t.me/om_karjok untuk requests\natau pertanyaan, atau hubungi https://t.me/CRABS_ID\nThanks :*\n\n')
+def setup():
+    try:
+        os.mkdir('/data/data/com.termux/files/home/.termux')
+    except:
+        pass
+    key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+    open('/data/data/com.termux/files/home/.termux/termux.properties','w').write(key)
+    os.system('termux-reload-settings')
 
+
+def banner():
+    os.system('clear')
+    print(a+'Shortcut for help you'.center(40))
+    print(b+'Karjok Pangesty'.center(40))
+    print("".join([i for i in "\n"*3]))
+
+
+if __name__=='__main__':
+    banner()
+    from threading import Thread as td
+    t = td(target=setup)
+    t.start()
+    while t.is_alive():
+        for i in "-\|/-\|/":
+            print('\rPlease wait '+i+' ',end="",flush=True)
+            sleep(0.1)
+    banner()
+    print(c+'Silahkan hubungi '+a+'https://t.me/om_karjok'+c+' jika ada yang mau di bicarakan terkait tool ini, bisnis atau sekedar bertanya kabar. :v\nTerimakasih ^_^')
 
 # ini cuma shortcut buat bantu para nub
 # karjok pangesty
